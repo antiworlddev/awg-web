6bab8d947e369bd14d0e2b283b39e10f647e77e0
gentlegiantafricatech <gentlegiantafrica@gmail.com>
2026-04-11 17:31:58 +0100

    fix image glitch

--- app/ui/hero-grid.tsx
+++ app/ui/hero-grid.tsx
@@ -4,7 +4,7 @@ export default function HeroGrid() {
   return (
     <div className="flex lg:flex-row flex-col-reverse lg:items-start items-center justify-between py-6 w-full lg:mt-6">
       <div className="flex flex-col lg:justify-start items-center lg:items-start w-full">
-        <Image src="/gang/gang10.JPG" width={600} height={350} alt="gang12" />
+        <Image src="/gang/gang10.jpg" width={600} height={350} alt="gang12" />
 
         <p className="tracking-widest lg:text-[80px] lg:self-start self-center text-4xl font-black lg:mt-10 mt-6">
           GANGSTARS
@@ -15,7 +15,7 @@ export default function HeroGrid() {
           ANTIWORLD
         </p>
 
-        <Image src="/gang/gang9.JPG" width={600} height={350} alt="gang12" />
+        <Image src="/gang/gang9.jpg" width={600} height={350} alt="gang12" />
       </div>
     </div>
   );
